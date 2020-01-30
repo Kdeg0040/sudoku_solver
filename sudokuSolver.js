847417d55586e57c43e0b68c4c0d72ccac74831a
karlo <deguzmankarlo@gmail.com>
2020-01-30 17:58:15 +0000

    Converts input string into 9x9 grid

--- sudokuSolver.js
+++ sudokuSolver.js
@@ -1,4 +1,13 @@
-function Board() {
-
+function Board(input) {
+  this.input = input;
+  this.grid = null;
 }
+
+Board.prototype.makeGrid = function(input) {
+  rows = input.match(/.{1,9}/g);
+  splitToInt = rows.map( x => x.split('').map(Number));
+  this.grid = splitToInt;
+}
+
+
 module.exports = Board;
--- sudokuSolver.test.js
+++ sudokuSolver.test.js
@@ -5,4 +5,17 @@ describe('board', () => {
     const board = new Board();
     expect(board).toBeDefined();
   });
+
+  it('can allow input of known numbers', () => {
+    const input = '000005021000000049931000500065030000400517002000040750007000983590000000320700000'
+    const board = new Board(input);
+    expect(board.input.length).toBe(81);
+  });
+
+  it('can draw a 9x9 grid', () => {
+    const blank = '000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+    const board = new Board(blank);
+    board.makeGrid(blank);
+    expect(board.grid.length).toBe(9);
+  });
 });
