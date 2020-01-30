2b711e8792d9f7ecb9a8ce197e0bcea522e5f2e6
karlo <deguzmankarlo@gmail.com>
2020-01-30 20:11:35 +0000

    Can identify blank cells

--- sudokuSolver.js
+++ sudokuSolver.js
@@ -1,6 +1,7 @@
 function Board(input) {
   this.input = input;
   this.grid = null;
+  this.blanks = [];
 }
 
 Board.prototype.makeGrid = function(input) {
@@ -9,5 +10,14 @@ Board.prototype.makeGrid = function(input) {
   this.grid = splitToInt;
 }
 
+Board.prototype.findBlanks = function() {
+  for (let row = 0; row <= 8; row++) {
+    for (let col = 0; col <= 8; col++) {
+      if (this.grid[row][col] == 0) {
+        this.blanks.push([row,col])
+      }
+    }
+  }
+}
 
 module.exports = Board;
--- sudokuSolver.test.js
+++ sudokuSolver.test.js
@@ -9,13 +9,30 @@ describe('board', () => {
   it('can allow input of known numbers', () => {
     const input = '000005021000000049931000500065030000400517002000040750007000983590000000320700000'
     const board = new Board(input);
-    expect(board.input.length).toBe(81);
+    expect(board.input.length).toEqual(81);
   });
 
   it('can draw a 9x9 grid', () => {
     const blank = '000000000000000000000000000000000000000000000000000000000000000000000000000000000'
     const board = new Board(blank);
     board.makeGrid(blank);
-    expect(board.grid.length).toBe(9);
+    expect(board.grid.length).toEqual(9);
+  });
+
+  it('can identify position of blank cells', () => {
+    const board = new Board();
+    board.grid = [
+      [0, 2, 3, 4, 5, 6, 7, 8, 9],
+      [4, 0, 6, 7, 8, 9, 1, 2, 3],
+      [7, 8, 0, 1, 2, 3, 4, 5, 6],
+      [2, 3, 4, 0, 6, 7, 8, 9, 1],
+      [5, 6, 7, 8, 0, 1, 2, 3, 4],
+      [8, 9, 1, 2, 3, 0, 5, 6, 7],
+      [3, 4, 5, 6, 7, 8, 0, 1, 2],
+      [6, 7, 8, 9, 1, 2, 3, 0, 5],
+      [9, 1, 2, 3, 4, 5, 6, 7, 0]
+    ]
+    board.findBlanks();
+    expect(board.blanks).toEqual([[0,0], [1,1], [2,2], [3,3], [4,4], [5,5], [6,6], [7,7], [8,8]]);
   });
 });
