643c1ad034ed149566e84d9b5f8f37ebeff7f847
karlo <deguzmankarlo@gmail.com>
2020-01-31 16:22:45 +0000

    Consolidate all cell validations tests

--- sudokuSolver.test.js
+++ sudokuSolver.test.js
@@ -30,7 +30,7 @@ describe('board', () => {
   });
 
 
-  it('can check if a value is valid on a given row or column', () => {
+  it('can check if a value is valid on a given row, column and sub grid', () => {
     const board = new Board();
     board.grid = [
       [0, 0, 0, 0, 0, 5, 0, 2, 1],
@@ -47,23 +47,7 @@ describe('board', () => {
     expect(board.validRow(0, 3)).toBe(true);
     expect(board.validColumn(0, 9)).toBe(false);
     expect(board.validColumn(0, 1)).toBe(true);
-  });
-
-  it('can check if a value within a cells sub grid', () => {
-    const board = new Board();
-    board.grid = [
-      [0, 0, 0, 0, 0, 5, 0, 2, 1],
-      [0, 0, 0, 0, 0, 0, 0, 4, 9],
-      [9, 3, 1, 0, 0, 0, 5, 0, 0],
-      [0, 6, 5, 0, 3, 0, 0, 0, 0],
-      [4, 0, 0, 5, 1, 7, 0, 0, 2],
-      [0, 0, 0, 0, 4, 0, 7, 5, 0],
-      [0, 0, 7, 0, 0, 0, 9, 8, 3],
-      [5, 9, 0, 0, 0, 0, 0, 0, 0],
-      [3, 2, 0, 7, 0, 0, 0, 0, 0]
-    ]
     expect(board.validSubGrid(0, 0, 1)).toBe(false);
     expect(board.validSubGrid(3, 2, 9)).toBe(true);
-
   });
 });
