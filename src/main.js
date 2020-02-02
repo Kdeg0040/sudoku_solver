f605f5bd1577c32e6c5a1dc6eeb641cc4435d4ea
karlo <deguzmankarlo@gmail.com>
2020-02-02 18:15:51 +0000

    Can write board values to table

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
+(function renderNumButtons() {
+  for (let i = 1; i < 10; i++) {
+    document.getElementById('num_buttons').innerHTML += 
+    "<button onclick='numButtons(this.value)' value=" + i + ">[" + i + "]</button>"
+  }
+})();
+
+function numButtons(val) {
+  console.log(val)
+}
+
+function renderBoard(board) {
+  boardDiv = document.getElementById("board");
+  table = document.createElement('TABLE');
+  tableBody = document.createElement('TBODY');
+  table.appendChild(tableBody);
+  for (let i = 0; i < 9; i++) {
+    tr = document.createElement('TR');
+    tableBody.appendChild(tr);
+    for (let j = 0; j < 9; j++) {
+      td = document.createElement('TD');
+      if (board.grid[i][j] != 0) {
+        td.appendChild(document.createTextNode(board.grid[i][j]));
+      }
+      tableBody.appendChild(td);
+    }
+  }
+  boardDiv.appendChild(table);
+}
+
 function newBoard() {
   const blank = 
   '000000000000000000000000000000000000000000000000000000000000000000000000000000000'
@@ -6,30 +36,3 @@ function newBoard() {
   renderBoard(board);
 }
 newBoard();
-
-(function renderNumButtons() {
-  for (let i = 1; i < 10; i++) {
-    document.getElementById('num_buttons').innerHTML += 
-    "<button onclick='numButtons(this.value)' value=" + i + ">[" + i + "]</button>"
-  }
-})();
-
-function renderBoard(board) {
-  board = document.getElementById("board");
-  table = document.createElement('TABLE');
-  tableBody = document.createElement('TBODY');
-  table.appendChild(tableBody);
-  for (let i = 0; i < 9; i++) {
-    tr = document.createElement('TR');
-    tableBody.appendChild(tr);
-    for (let i = 0; i < 9; i++) {
-      td = document.createElement('TD');
-      tableBody.appendChild(td);
-    }
-  }
-  board.appendChild(table);
-}
-
-function numButtons(val) {
-  console.log(val)
-}
