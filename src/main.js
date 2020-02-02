e1aef085a16851cb99e1ca56fee5430b6f4a8c7b
karlo <deguzmankarlo@gmail.com>
2020-02-02 17:31:47 +0000

    Draw 9x9 table

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 function newBoard() {
-  document.getElementById('board').innerHTML +=
-  '<p>This is a new board</p>'
+  const blank = 
+  '000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+  let board = new Board(blank);
+  board.makeGrid(blank);
+  renderBoard(board);
 }
+newBoard();
 
 (function renderNumButtons() {
   for (let i = 1; i < 10; i++) {
@@ -9,6 +13,22 @@ function newBoard() {
     "<button onclick='numButtons(this.value)' value=" + i + ">[" + i + "]</button>"
   }
 })();
+
+function renderBoard(board) {
+  board = document.getElementById("board");
+  table = document.createElement('TABLE');
+  tableBody = document.createElement('TBODY');
+  table.appendChild(tableBody);
+  for (let i = 0; i < 9; i++) {
+    tr = document.createElement('TR');
+    tableBody.appendChild(tr);
+    for (let i = 0; i < 9; i++) {
+      td = document.createElement('TD');
+      tableBody.appendChild(td);
+    }
+  }
+  board.appendChild(table);
+}
 
 function numButtons(val) {
   console.log(val)
