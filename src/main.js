12c310924726036345817f69ef656edd5968bbf6
karlo <deguzmankarlo@gmail.com>
2020-02-02 15:48:13 +0000

    Add iffe to render number buttons on page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-const Board = require('../src/sudokuSolver');
-
 function newBoard() {
-  document.getElementById("board").innerHTML +=
-  "<p>This is a new board</p>"
+  document.getElementById('board').innerHTML +=
+  '<p>This is a new board</p>'
+}
+
+(function renderNumButtons() {
+  for (let i = 1; i < 10; i++) {
+    document.getElementById('num_buttons').innerHTML += 
+    "<button onclick='numButtons(this.value)' value=" + i + ">[" + i + "]</button>"
+  }
+})();
+
+function numButtons(val) {
+  console.log(val)
 }
