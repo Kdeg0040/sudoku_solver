4db1216ecc19f71354da8e0a64b63f56db0b32fb
karlo <deguzmankarlo@gmail.com>
2020-02-04 18:10:14 +0000

    Can clear set number of cells from complete board

--- spec/sudokuSolver.test.js
+++ spec/sudokuSolver.test.js
@@ -93,4 +93,25 @@ describe('board', () => {
       board.solve();
     }).toThrow('No solution found')
   });
+
+  it('can clear cells from a complete board', () => {
+    board = new Board();
+    remainingCells = 25;
+    board.grid = [
+      [8, 7, 4, 9, 6, 5, 3, 2, 1],
+      [6, 5, 2, 1, 7, 3, 8, 4, 9],
+      [9, 3, 1, 4, 2, 8, 5, 7, 6],
+      [7, 6, 5, 8, 3, 2, 1, 9, 4],
+      [4, 8, 9, 5, 1, 7, 6, 3, 2],
+      [2, 1, 3, 6, 4, 9, 7, 5, 8],
+      [1, 4, 7, 2, 5, 6, 9, 8, 3],
+      [5, 9, 6, 3, 8, 4, 2, 1, 7],
+      [3, 2, 8, 7, 9, 1, 4, 6, 5]
+    ]
+    board.findBlanks();
+    expect(board.blanks.length).toEqual(0);
+    board.clearCells(remainingCells);
+    expect(board.blanks.length).toBeGreaterThan(0);
+    expect(board.blanks.length).toEqual(81-remainingCells);
+  });
 });
